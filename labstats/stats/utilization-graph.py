38eb03aa6443c9fc34df05b4f555e00b0c32e295
William Ho <willh@ocf.berkeley.edu>
2014-10-18 10:34:12 -0700

    Utilization graphs: Improve looks, add arg parsing
    Switch to line charts and add grid lines
    Add a weighted sliding average to smooth out input data
    Graph size now depends on the "graph_dpi" variable in the script
    Add command-line options for date and output filename

--- labstats/stats/utilization-graph.py
+++ labstats/stats/utilization-graph.py
@@ -3,12 +3,15 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import os
 
 from labstats import settings, db
 from labstats.stats import utilization
 from datetime import datetime, timedelta, date
 
-def generate_image(profiles, hosts, start, end):
+graph_dpi=75
+
+def generate_image(profiles, hosts, start, end, dest):
 	"""Generates an image representing usage of the lab at minute resolution
 	from a set of profiles, each representing a single machine."""
 
@@ -22,28 +25,40 @@ def generate_image(profiles, hosts, start, end):
 
 		sums.append(in_use)
 	
+	#Do a simple weighted moving average to smooth out the data
+	processed = [0] * len(sums)
+	for i in range(len(sums)):
+		for j in range(-2, 3):
+			processed[i] += 0 if (i+j < 0 or i+j >= len(sums)) else (3-abs(j))*sums[i+j]
+		processed[i] /= 9
+	sums = processed
 	h = lambda h: h if h <= 12 else h - 12
 	p = lambda h: "am" if h <= 11 else "pm"
 	hours = ["{}{}".format(h(hour), p(hour)) for hour in range(start.hour, start.hour + minutes // 60)]
 
-	plt.bar(range(minutes), sums, color="b")
+	x = list(range(minutes))
+	plt.figure(figsize=(10,6))
+	plt.grid(True)
+	plt.plot(x, sums, color="k", linewidth=1.5)
 	plt.xlim(0, minutes)
 	plt.xticks(np.arange(0, minutes, 60), hours)
 	plt.xlabel("Time")
-	plt.ylabel("Computers in Use")
 	plt.ylim(0, len(hosts))
+	plt.ylabel("Computers in Use")
 
-	plt.title("Lab Utilization {}".format(start.strftime("%a %b %d, %Y")))
+	plt.title("Average Lab Utilization {}".format(start.strftime("%a %b %d, %Y")))
 
-	plt.savefig(dest)
+	plt.savefig(dest, dpi=graph_dpi, bbox_inches='tight')
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Generate utilization graphs")
-	parser.add_argument("--date", type=str, default=datetime.now().isoformat(),
+	parser.add_argument("--date", type=str, dest="lookup_date", default=date.today().isoformat(),
 			help="Date to generate graphs for")
+	parser.add_argument("--destination", type=str, dest="dest", default=os.path.join(os.getcwd(), "labstats.png"),
+			help="Destination file name and path for output graph")
+	args = parser.parse_args()
 
-	yesterday = date.today() - timedelta(1)
-	day = date.today() if datetime.now().hour >= 18 else yesterday
+	day = datetime.strptime(args.lookup_date, "%Y-%m-%d")
 
 	start = datetime(day.year, day.month, day.day, 9) # 9am
 	end = datetime(day.year, day.month, day.day, 18) # 6pm
