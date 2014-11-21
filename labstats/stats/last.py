be1dc5969de6d06ac9a4bc5ad11cb10c2615ca8b
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-11-21 00:11:06 -0800

    Add last.py to see login history for user

---
+++ labstats/stats/last.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Print login record for a user
+
+import sys, os, datetime
+import labstats.update, labstats.db
+
+HOST_SUFFIX = '.ocf.berkeley.edu'
+
+cnx = labstats.db.get_connection()
+cursor = cnx.cursor()
+
+if len(sys.argv) != 2:
+	print("usage: {} user".format(sys.argv[0]))
+	sys.exit(1)
+
+query = "SELECT `host`, `start`, `end` FROM `session` WHERE `user` = %s ORDER BY `start` DESC"
+res = cursor.execute(query, (sys.argv[1],))
+
+for record in cursor:
+	host, start, end = record
+	if host.endswith(HOST_SUFFIX):
+		host = host[:len(host) - len(HOST_SUFFIX)]
+	print("{} from {} to {}".format(host, start, end))
