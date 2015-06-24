99c9fd59b81b838b12975e8169d9b8955fe92788
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-06-24 16:55:51 -0700

    check_toner: use ocflib instead of pysnmp directly

--- printing/check_toner/check_pages.py
+++ printing/check_toner/check_pages.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 import sys
-from pysnmp.entity.rfc3413.oneliner import cmdgen
+from ocflib.printing.printers import get_lifetime_pages
 
-# Do NOT ask me why this is off from the amount displayed on the printer's own web page.
-oidPagesPrinted = "1.3.6.1.2.1.43.10.2.1.4.1.1"
-
-errorIndication, errorStatus, errorIndex, varBinds = cmdgen.CommandGenerator().getCmd(
-    cmdgen.CommunityData('my-agent', 'public', 0),
-    cmdgen.UdpTransportTarget((sys.argv[1], 161)),
-    oidPagesPrinted
-    )
-
-print "/".join([str(var[1]) for var in varBinds])
+if __name__ == '__main__':
+    print(get_lifetime_pages(sys.argv[1]))
--- printing/check_toner/check_toner.py
+++ printing/check_toner/check_toner.py
@@ -1,18 +1,6 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 import sys
-from pysnmp.entity.rfc3413.oneliner import cmdgen
+from ocflib.printing.printers import get_toner
 
-# http://forums.cacti.net/about2771.html
-oidTonerRemaining = "1.3.6.1.2.1.43.11.1.1.9.1.1"
-oidTonerMax       = "1.3.6.1.2.1.43.11.1.1.8.1.1"
-
-
-errorIndication, errorStatus, errorIndex, varBinds = cmdgen.CommandGenerator().getCmd(
-    cmdgen.CommunityData('my-agent', 'public', 0),
-    cmdgen.UdpTransportTarget((sys.argv[1], 161)),
-    oidTonerRemaining,
-    oidTonerMax
-    )
-
-print "/".join([str(var[1]) for var in varBinds])
+if __name__ == '__main__':
+    print("/".join(map(str, get_toner(sys.argv[1]))))
