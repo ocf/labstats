f136a81a209ea99079b2d3efdb9f332cb3488fc1
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-11-21 20:24:35 -0800

    Add `session_duration` and `session_duration_public` views
    The goal of creating public views is to be more explicit about what
    information we consider public. In particular, anyone can now query that
    table.

--- labstats/bin/init.py
+++ labstats/bin/init.py
@@ -8,14 +8,23 @@ cursor = cnx.cursor()
 print("Creating session table...")
 
 query = """
-	CREATE TABLE `session` (
-		`id` int NOT NULL AUTO_INCREMENT,
-		`host` varchar(255) NOT NULL,
-		`user` varchar(8) NOT NULL,
-		`start` datetime NOT NULL,
-		`end` datetime DEFAULT NULL,
-		`last_update` datetime,
-		PRIMARY KEY (`id`)
-	) ENGINE=InnoDB"""
+CREATE TABLE `session` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `host` varchar(255) NOT NULL,
+        `user` varchar(8) NOT NULL,
+        `start` datetime NOT NULL,
+        `end` datetime DEFAULT NULL,
+        `last_update` datetime,
+        PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE VIEW `session_duration` AS
+    SELECT *, timediff(`end`, `start`) AS `duration` FROM `session`;
+
+CREATE VIEW `session_duration_public` AS
+    SELECT `id`, `host`, `start`, `end`, `duration` FROM `session_duration`;
+
+GRANT SELECT ON `ocfstats`.`session_duration_public` TO 'anonymous'@'%';
+"""
 
 cursor.execute(query)
