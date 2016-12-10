2967835ce394bd8a5e7f9dde143b0655d23901f5
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-12-09 23:56:41 -0800

    Write toner and lifetime pages to MySQL as well as the file

--- printing/current_toner.py
+++ printing/current_toner.py
@@ -5,23 +5,39 @@ import time
 from ocflib.printing.printers import get_toner
 from ocflib.printing.printers import PRINTERS
 
-dest_dir = '/opt/stats/var/printing/oracle/'
-suffix = '.csv'
+import labstats.db
 
-for target in PRINTERS:
-    now = str(time.time())
 
-    try:
-        toner = get_toner(target)
-    except OSError as ex:
-        print('Error reading data from {}, continuing to next...'.format(target))
-        print('\t{}'.format(ex))
-        continue
+if __name__ == '__main__':
+    dest_dir = '/opt/stats/var/printing/oracle/'
+    suffix = '.csv'
 
-    with open(dest_dir + target + suffix, 'a+') as file:
-        out_str = ','.join(map(str, (now,) + toner)) + '\n'
-        file.write(out_str)
-        print('Updated {target} with new value: {out_str}'.format(
-            target=target,
-            out_str=out_str,
-        ))
+    for target in PRINTERS:
+        now = str(time.time())
+
+        try:
+            toner = get_toner(target)
+        except OSError as ex:
+            print('Error reading data from {}, continuing to next...'.format(target))
+            print('\t{}'.format(ex))
+            continue
+
+        with open(dest_dir + target + suffix, 'a+') as file:
+            out_str = ','.join(map(str, (now,) + toner)) + '\n'
+            file.write(out_str)
+            print('Updated {target} with new value: {out_str}'.format(
+                target=target,
+                out_str=out_str,
+            ))
+
+        value, max_ = toner
+        c = labstats.db.get_connection()
+        cursor = c.cursor()
+        cursor.execute(
+            (
+                'INSERT INTO `printer_toner` (`date`, `printer`, `value`, `max`)'
+                '   VALUES (CURRENT_TIMESTAMP(), %s, %s, %s)'
+            ),
+            (target, value, max_),
+        )
+        c.commit()
--- printing/lifetime_pages.py
+++ printing/lifetime_pages.py
@@ -5,24 +5,38 @@ import time
 from ocflib.printing.printers import get_lifetime_pages
 from ocflib.printing.printers import PRINTERS
 
-dest_dir = '/opt/stats/var/printing/history/'
-suffix = '.csv'
+import labstats.db
 
-for target in PRINTERS:
-    now = str(time.time())
 
-    try:
-        toner = [get_lifetime_pages(target)]
-    except OSError as ex:
-        print('Error reading data from {}, continuing to next...'.format(target))
-        print('\t{}'.format(ex))
-        continue
+if __name__ == '__main__':
+    dest_dir = '/opt/stats/var/printing/history/'
+    suffix = '.csv'
 
-    with open(dest_dir + target + suffix, 'a+') as file:
-        toner.insert(0, now)
-        out_str = ','.join(map(str, toner)) + '\n'
-        file.write(out_str)
-        print('Updated {target} with new value: {out_str}'.format(
-            target=target,
-            out_str=out_str,
-        ))
+    for target in PRINTERS:
+        now = str(time.time())
+
+        try:
+            lifetime_pages = get_lifetime_pages(target)
+        except OSError as ex:
+            print('Error reading data from {}, continuing to next...'.format(target))
+            print('\t{}'.format(ex))
+            continue
+
+        with open(dest_dir + target + suffix, 'a+') as file:
+            out_str = ','.join(map(str, (now, lifetime_pages))) + '\n'
+            file.write(out_str)
+            print('Updated {target} with new value: {out_str}'.format(
+                target=target,
+                out_str=out_str,
+            ))
+
+        c = labstats.db.get_connection()
+        cursor = c.cursor()
+        cursor.execute(
+            (
+                'INSERT INTO `printer_pages` (`date`, `printer`, `value`)'
+                '   VALUES (CURRENT_TIMESTAMP(), %s, %s)'
+            ),
+            (target, lifetime_pages),
+        )
+        c.commit()
